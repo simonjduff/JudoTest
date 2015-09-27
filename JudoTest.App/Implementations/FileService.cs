05e6eaf831acf79a7aff3a44fc46562bc636b247
Simon Duff <simon.j.duff@gmail.com>
2015-09-27 08:39:16 +0100

    added wrapper for static File

---
+++ JudoTest.App/Implementations/FileService.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using JudoTest.App.Interfaces;
+
+namespace JudoTest.App.Implementations
+{
+    public class FileService : IFileService
+    {
+        public string ReadAllText(string filename)
+        {
+            return File.ReadAllText(filename);
+        }
+    }
+}
