33b472e653e9b830048d90e16b981a0e404baa71
Simon Duff <simon.j.duff@gmail.com>
2015-09-27 09:04:59 +0100

    Tests and implementation for word splitter

---
+++ JudoTest.App/Implementations/WordSplitter.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using JudoTest.App.Interfaces;
+
+namespace JudoTest.App.Implementations
+{
+    public class WordSplitter : IWordSplitter
+    {
+        private static readonly Regex WordRegex = new Regex(@"(?<word>\w+)", RegexOptions.Compiled);
+        public string[] Split(string inputData)
+        {
+            var matches = WordRegex.Matches(inputData);
+
+            return (from Match match in matches where match.Success && match.Groups["word"].Success select match.Groups["word"].Value).ToArray();
+        }
+    }
+}
---
+++ JudoTest.Tests/WordSplitterTests.cs
@@ -0,0 +1,51 @@
+﻿using JudoTest.App.Interfaces;
+using NUnit.Framework;
+using System.Collections.Generic;
+using JudoTest.App.Implementations;
+
+namespace JudoTest.Tests
+{
+    [TestFixture]
+    public class WordSplitterTests
+    {
+        private IWordSplitter _wordSplitter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _wordSplitter = new WordSplitter();
+        }
+
+        [Test]
+        public void WordSplitter_SplitsWordsSeparatedBySpaces()
+        {
+            var words = "one two three one";
+            var result = _wordSplitter.Split(words);
+            Assert.AreEqual(new[] { "one", "two", "three", "one" }, result);
+        }
+
+        [Test]
+        public void WordSplitter_WhenEmptyInput_ReturnsEmptyArray()
+        {
+            var words = string.Empty;
+            var result = _wordSplitter.Split(words);
+            Assert.AreEqual(new string[0], result);
+        }
+
+        [Test]
+        public void WordSplitter_IgnoresSpecialCharacters()
+        {
+            var words = "one 'two' three! one:";
+            var result = _wordSplitter.Split(words);
+            Assert.AreEqual(new[] { "one", "two", "three", "one" }, result);
+        }
+
+        [Test]
+        public void WordSplitter_ChecksAcrossNewlines()
+        {
+            var words = "one two\r\nthree one";
+            var result = _wordSplitter.Split(words);
+            Assert.AreEqual(new[] { "one", "two", "three", "one" }, result);
+        }
+    }
+}
